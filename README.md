5b4082923a9f875ec3df43e92faee88c5e43ed62
tonmoy-pk <tonmoykumar78@gmail.com>
2023-01-20 19:54:47 +0600

    update readme and build for live

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# tonmoy-pk.github.io
+# Mails will not be shown because of firebase authentication rules
 
 ### This mail application need more work to do for adding more UI and functionality
---
+++ build/precache-manifest.ccd54460b0ba8d39fe282f2d386eb287.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "700456bb12930876c598",
+    "url": "/mail-application-react/static/css/main.e3fd60fd.chunk.css"
+  },
+  {
+    "revision": "700456bb12930876c598",
+    "url": "/mail-application-react/static/js/main.700456bb.chunk.js"
+  },
+  {
+    "revision": "337678c91db3c3480b4a",
+    "url": "/mail-application-react/static/js/1.337678c9.chunk.js"
+  },
+  {
+    "revision": "7ea2c467fb73a9732ccb",
+    "url": "/mail-application-react/static/js/runtime~main.7ea2c467.js"
+  },
+  {
+    "revision": "4a70fa01d95cc6f52dd64be22278c50e",
+    "url": "/mail-application-react/index.html"
+  }
+];
