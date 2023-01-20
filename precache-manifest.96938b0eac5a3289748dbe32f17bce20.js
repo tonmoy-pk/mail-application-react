904af336a443ba92cf08fdb9bda7324b0052024f
tonmoy-pk <tonmoykumar78@gmail.com>
2023-01-20 19:30:00 +0600

    Updates

---
+++ precache-manifest.96938b0eac5a3289748dbe32f17bce20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "700456bb12930876c598",
+    "url": "/mail-application/static/css/main.e3fd60fd.chunk.css"
+  },
+  {
+    "revision": "700456bb12930876c598",
+    "url": "/mail-application/static/js/main.700456bb.chunk.js"
+  },
+  {
+    "revision": "0f51f24425db5a9be7a4",
+    "url": "/mail-application/static/js/1.0f51f244.chunk.js"
+  },
+  {
+    "revision": "b22999e4ddc2bec98e16",
+    "url": "/mail-application/static/js/runtime~main.b22999e4.js"
+  },
+  {
+    "revision": "67ef0b397336df8f03a467f0a13fbd18",
+    "url": "/mail-application/index.html"
+  }
+];
